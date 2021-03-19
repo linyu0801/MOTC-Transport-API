39e9277261d9ddb9a8350f0219fab7c7bb79b3ab
fish <spe9g0908@gmail.com>
2021-03-19 14:49:16 +0800

    git

--- src/component/MyNavbar.js
+++ src/component/MyNavbar.js
@@ -58,8 +58,7 @@ function MyNavbar(props) {
                 className="nav-member-link "
               >
                 {city.map((value, index) => (
-                  {/* <Nav.Link href={`/scenicSpot/${city[index]}`}> */}
-                    <Nav.Link href={`/scenicSpot/${city[index]}`}>
+                  <Nav.Link href={`/scenicSpot/${city[index]}`}>
                     {`${city[index]}`}
                     {/* <NavDropdown.Item
                       // as={NavLink}
